d5d1413813f2fe291eee1e1c09d96bf6c8d41c29
smyoo <proysm5154@naver.com>
2024-09-07 15:08:32 +0900

    update : 백엔드 초기 세팅

---
+++ back/src/test/java/com/thered/stocksignal/StocksignalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thered.stocksignal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
