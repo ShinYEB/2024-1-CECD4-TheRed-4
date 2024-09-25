0a1d9e6e169ee9b98d7794c1a7836903d7d8ac6d
ShinYEB <91859242+ShinYEB@users.noreply.github.com>
2024-09-25 16:37:21 +0900

    [FEAT]: android 시나리오 추가 구현
    android 시나리오 추가 구현
    
    ISSUE #3

--- app/src/main/java/com/thered/stocksignal/Presentation/Main/MainActivity.kt
+++ app/src/main/java/com/thered/stocksignal/Presentation/Main/MainActivity.kt
@@ -18,6 +18,7 @@ import com.thered.stocksignal.Presentation.ChatBot.ChatBotActivity
 import com.thered.stocksignal.Presentation.Home.HomeFragment
 import com.thered.stocksignal.Presentation.Home.StockCoverFragment
 import com.thered.stocksignal.Presentation.Mypage.MypageFragment
+import com.thered.stocksignal.Presentation.NewScenario.NewScenarioActivity
 import com.thered.stocksignal.Presentation.Search.SearchFragment
 import com.thered.stocksignal.R
 
@@ -48,6 +49,8 @@ class MainActivity : AppCompatActivity() {
                 true
             }
         }
+        val intent = Intent(this, NewScenarioActivity::class.java)
+        startActivity(intent)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
---
+++ app/src/main/java/com/thered/stocksignal/Presentation/NewScenario/NewScenarioViewModel.kt
@@ -0,0 +1,7 @@
+package com.thered.stocksignal.Presentation.NewScenario
+
+import androidx.lifecycle.ViewModel
+
+class NewScenarioViewModel: ViewModel() {
+
+}
