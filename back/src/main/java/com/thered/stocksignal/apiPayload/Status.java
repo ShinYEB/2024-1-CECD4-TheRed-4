0e5d1ebc4a2d3328faf4afc61930cf1b43f4fa22
smyoo <proysm5154@naver.com>
2024-09-27 02:06:28 +0900

    feat : 주식 매수/매도 API 응답 구현 #7

--- back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
+++ back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
@@ -26,7 +26,8 @@ public enum Status {
      * 404 :
      * ...
      */
-
+    TRADE_BUY_SUCCESS("200", "SUCCESS", "매수를 성공했습니다."),
+    TRADE_SELL_SUCCESS("200", "SUCCESS", "매도를 성공했습니다."),
     LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다.");
 
     private final String code;
