6a0960880339798a1376450b52f35181f8a9ca79
smyoo <proysm5154@naver.com>
2024-09-27 01:35:20 +0900

    feat : 회원정보 API 응답 구현

--- back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
+++ back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
@@ -27,7 +27,10 @@ public enum Status {
      * ...
      */
 
-    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다.");
+    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다."),
+    GET_USERINFO_SUCCESS("200", "SUCCESS", "회원 정보가 조회되었습니다."),
+    SET_USERINFO_SUCCESS("200", "SUCCESS", "회원 정보가 수정되었습니다."),
+    NICKNAME_SUCCESS("200", "SUCCESS", "사용 가능한 닉네임입니다.");
 
     private final String code;
     private final String result;
