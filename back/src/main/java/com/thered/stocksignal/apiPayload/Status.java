b2c12a0b30f798c8643b1b4413900ba987e90b89
price126 <hwjun12@gmail.com>
2024-09-27 19:18:32 +0900

    [FEAT] 주가 정보 조회 API 응답 구현

--- back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
+++ back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
@@ -27,7 +27,12 @@ public enum Status {
      * ...
      */
 
-    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다.");
+    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다."),
+    STOCK_CODE_SUCCESS("200", "SUCCESS", "종목 코드 조회에 성공했습니다."),
+    CURRENT_DATA_SUCCESS("200", "SUCCESS", "현재가 조회에 성공했습니다."),
+    MINUTE_DATA_SUCCESS("200", "SUCCESS", "분봉 조회에 성공했습니다."),
+    PERIOD_DATA_SUCCESS("200", "SUCCESS", "기간별 조회에 성공했습니다."),
+    AI_PREDICTION_SUCCESS("200", "SUCCESS", "AI 예상 주가 조회에 성공했습니다.");
 
     private final String code;
     private final String result;
