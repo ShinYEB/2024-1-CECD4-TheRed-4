65a2700f9770cd42e96fd8f3b3c35eb569d56b18
price126 <hwjun12@gmail.com>
2024-09-27 21:18:04 +0900

    [FEAT] 챗봇 질의응답 API 응답 구현

--- back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
+++ back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
@@ -27,7 +27,8 @@ public enum Status {
      * ...
      */
 
-    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다.");
+    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다."),
+    QUESTION_FOUND("200", "SUCCESS", "답변 정보를 찾았습니다.");
 
     private final String code;
     private final String result;
