485fabe7c3bcf73de7675237bc81485a53eb4875
price126 <hwjun12@gmail.com>
2024-09-27 20:33:58 +0900

    [FEAT] 회사 정보 조회 API 응답 구현

--- back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
+++ back/src/main/java/com/thered/stocksignal/apiPayload/Status.java
@@ -27,7 +27,8 @@ public enum Status {
      * ...
      */
 
-    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다.");
+    LOGIN_SUCCESS("200", "SUCCESS", "로그인이 완료되었습니다."),
+    COMPANY_INFO_SUCCESS("200", "SUCCESS", "회사 정보 조회에 성공하였습니다.");
 
     private final String code;
     private final String result;
