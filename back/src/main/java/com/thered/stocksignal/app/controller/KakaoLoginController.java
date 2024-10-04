eea0124355a36b6f76ce2a62def80d5eb79e37c7
smyoo <proysm5154@naver.com>
2024-10-04 22:59:39 +0900

    feat : 카카오 인가코드, 토큰, 사용자 정보 가지고오기

--- back/src/main/java/com/thered/stocksignal/app/controller/KakaoLoginController.java
+++ back/src/main/java/com/thered/stocksignal/app/controller/KakaoLoginController.java
@@ -2,8 +2,10 @@ package com.thered.stocksignal.app.controller;
 
 import com.thered.stocksignal.apiPayload.ApiResponse;
 import com.thered.stocksignal.apiPayload.Status;
-import com.thered.stocksignal.app.dto.KakaoLoginDto;
+import com.thered.stocksignal.app.dto.kakao.KakaoLoginDto;
+import com.thered.stocksignal.domain.entity.User;
 import com.thered.stocksignal.service.kakao.KakaoLoginService;
+import com.thered.stocksignal.service.user.UserAccountService;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +20,30 @@ import io.swagger.v3.oas.annotations.Operation;
 public class KakaoLoginController {
 
     private final KakaoLoginService kakaoLoginService;
+    private final UserAccountService userAccountService;
 
-    @Operation(summary = "프론트로부터 카카오 인가코드 전달받기")
+    @Operation(summary = "프론트로부터 카카오 인가코드 전달받기 좀 돼라라라라ㅏㄹ")
     @Parameter(name = "code", description = "카카오에서 받은 인카코드, RequestParam")
     @GetMapping("/login")
     public ApiResponse<?> kakaoLoginCode(@RequestParam("code") String code){
-        KakaoLoginDto.LoginResponseDto dto = new KakaoLoginDto.LoginResponseDto().builder()
-                .userId(1)
-                .token("1")
-                .build();
-        return ApiResponse.onSuccess(Status.LOGIN_SUCCESS, dto);
+
+        String token = kakaoLoginService.getKakaoToken(code);
+        KakaoLoginDto.KakaoUserInfoDto kakaoUserInfoDto = kakaoLoginService.getKakaoUserInfo(token);
+
+
+        return ApiResponse.onSuccess(Status.LOGIN_SUCCESS, kakaoUserInfoDto);
+    }
+
+    @Operation(summary = "백엔드에서 인가코드 확인용으로, 사용하지 않는 API입니다.")
+    @GetMapping("/callback")
+    public String testInga(@RequestParam("code") String code){
+        return code;
+    }
+
+    @Operation(summary = "백엔드에서 토큰 정보 확인용으로, 사용하지 않는 API입니다.")
+    @GetMapping("/valid")
+    public void testToken(@RequestParam("code") String token){
+        kakaoLoginService.isValidToken(token);
     }
 
 }
