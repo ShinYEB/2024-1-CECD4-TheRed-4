0b7ce117e46939c18ec8a160b020e9dfb447f581
price126 <hwjun12@gmail.com>
2024-10-11 16:19:20 +0900

    [FIX]빌드 에러 수정

--- back/src/test/java/com/thered/stocksignal/StocksignalApplicationTests.java
+++ back/src/test/java/com/thered/stocksignal/StocksignalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StocksignalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
