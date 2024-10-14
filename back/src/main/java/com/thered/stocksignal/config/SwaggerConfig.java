0eadf6875a31cc5179fe140b44cdb1776f816dbf
smyoo <proysm5154@naver.com>
2024-10-14 13:40:01 +0900

    prod : swagger 서버 추가

--- back/src/main/java/com/thered/stocksignal/config/SwaggerConfig.java
+++ back/src/main/java/com/thered/stocksignal/config/SwaggerConfig.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
 
 @Configuration
 public class SwaggerConfig {
@@ -14,6 +18,22 @@ public class SwaggerConfig {
                 .title("StockSignal API Document")
                 .version("v0.0.1")
                 .description("StockSignal API 명세서입니다.");
-        return new OpenAPI().components(new Components()).info(info);
+
+        Server httpServer = new Server()
+                .url("http://localhost:8080")
+                .description("HTTP server");
+
+        Server httpServer2 = new Server()
+                .url("http://pposiraun.com")
+                .description("HTTP server");
+
+        Server httpsServer = new Server()
+                .url("https://pposiraun.com")
+                .description("HTTPS server");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info)
+                .servers(List.of(httpServer, httpServer2, httpsServer));
     }
 }
