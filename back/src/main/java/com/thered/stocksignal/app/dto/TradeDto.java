ae6c0f7e50a6e631ea83287b92833262d2ea2204
price126 <hwjun12@gmail.com>
2024-10-27 19:24:06 +0900

    [UPDATE] 매수매도시 시장가/지정가 구분하고 가격 주도록 수정

--- back/src/main/java/com/thered/stocksignal/app/dto/TradeDto.java
+++ back/src/main/java/com/thered/stocksignal/app/dto/TradeDto.java
@@ -1,5 +1,6 @@
 package com.thered.stocksignal.app.dto;
 
+import com.thered.stocksignal.domain.enums.OrderType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TradeDto {
     public String scode; // 종목코드
-    public String week; // 주문수량
+    public OrderType orderType; // 지정가/시장가 구분
+    public Long price; // 주문단가
+    public Long week; // 주문수량
 }
